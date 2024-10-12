f9f7758c4979f56150b30aedf01de41365582701
kitti <tkt.ms@hotmail.com>
2024-10-12 16:22:13 +0700

    Test Firebase

--- Assets/Scripts/FirebaseRankingManager.cs
+++ Assets/Scripts/FirebaseRankingManager.cs
@@ -1,13 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Proyecto26;
+using SimpleJSON;
+using System.Linq;
 
 public class FirebaseRankingManager : MonoBehaviour
 {
+    public const string url = "https://moodengleaderboard-default-rtdb.asia-southeast1.firebasedatabase.app/";
+    public const string secret = "2S8Q9HGudjIZuK1nmJJo56249y2XB51sFCxRESgx";
+
+    [Header("Test")]
+    public int testNum;
+    [System.Serializable]
+    public struct TestData
+    {
+        public int num;
+        public string name;
+    }
+
+    public TestData testData = new TestData();
+
+    public void TestSetData()
+    {
+        string urlData = $"{url}.json?auth={secret}";
+
+        testData.name = "AAA";
+        testData.num = 1;
+
+        RestClient.Put<TestData>(urlData, testData).Then(response =>
+        {
+            Debug.Log("Upload Data Complete");
+        }).Catch(error =>
+        {
+            Debug.Log("Error on set to server");
+            Debug.Log(error.Message);
+        });
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TestSetData();
     }
 
     // Update is called once per frame
